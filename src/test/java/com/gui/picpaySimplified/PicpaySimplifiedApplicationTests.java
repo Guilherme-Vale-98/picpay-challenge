aacba167ea9c553cfe53e691a682c6a8519c15d9
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-05 00:30:09 -0300

    second commit

---
+++ src/test/java/com/gui/picpaySimplified/PicpaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.picpaySimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
