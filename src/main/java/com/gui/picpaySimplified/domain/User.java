5d056c975c109cf89d2f1fb3e04bf6dc3b109456
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-06 13:59:54 -0300

    Added User Repository

--- src/main/java/com/gui/picpaySimplified/domain/User.java
+++ src/main/java/com/gui/picpaySimplified/domain/User.java
@@ -10,11 +10,23 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
 public class User {
 	
 	@Id
